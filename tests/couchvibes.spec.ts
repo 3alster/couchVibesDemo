b5a615eafc16f1537d799826c2ceb2564af09bbe
Artur Pachin <3alster@gmail.com>
2023-03-21 13:56:38 +0300

    add screenshot before exiting

--- tests/couchvibes.spec.ts
+++ tests/couchvibes.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('beta couchvibes login and join event', async ({ page }) => {
+test('beta couchvibes login and join event', async ({ page },testInfo) => {
     await page.goto('https://beta.couchvibes.com/');
     await expect(page).toHaveURL('https://beta.couchvibes.com/public/page');
     await page.getByRole('button', { name: 'Log in' }).click();
@@ -15,6 +15,8 @@ test('beta couchvibes login and join event', async ({ page }) => {
     await expect(page.getByText('Danyo\'s room, Dennis Waberowski from Berlin, Deutschland')).toBeVisible();
     await expect(page.getByText('hihihi')).toBeVisible();
     await page.waitForTimeout(10000);
+    const screenshotBeforeExit = await page.screenshot();
+    await testInfo.attach('beforeExit', { body: screenshotBeforeExit, contentType: 'image/png' });
     await page.locator('.head-right > button:nth-child(3)').click();
     await page.getByRole('button', { name: 'Exit Stream' }).click();
 });
