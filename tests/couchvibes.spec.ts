9aad9cb575ae113e7a83e60da03b8b21f38ccfc0
Artur Pachin <3alster@gmail.com>
2023-03-21 15:49:35 +0300

    add screenshot before JOIN

--- tests/couchvibes.spec.ts
+++ tests/couchvibes.spec.ts
@@ -10,13 +10,14 @@ test('beta couchvibes login and join event', async ({ page },testInfo) => {
     await page.getByPlaceholder('Password').fill(process.env.C_PASS);
     await page.getByRole('button', { name: 'Login' }).click();
     await page.locator('.head-img').first().click();
+    await expect(page.getByRole('button', { name: 'Join Event' })).toBeVisible();
+    await testInfo.attach('beforeJoin', { body: await page.screenshot(), contentType: 'image/png' });
     await page.getByRole('button', { name: 'Join Event' }).click();
     await expect(page.getByText('Live Now')).toBeVisible();
     await expect(page.getByText('Danyo\'s room, Dennis Waberowski from Berlin, Deutschland')).toBeVisible();
-    await expect(page.getByText('hihihi')).toBeVisible();
+    //await expect(page.getByText('hihihi')).toBeVisible();
     await page.waitForTimeout(10000);
-    const screenshotBeforeExit = await page.screenshot();
-    await testInfo.attach('beforeExit', { body: screenshotBeforeExit, contentType: 'image/png' });
+    await testInfo.attach('beforeExit', { body: await page.screenshot(), contentType: 'image/png' });
     await page.locator('.head-right > button:nth-child(3)').click();
     await page.getByRole('button', { name: 'Exit Stream' }).click();
 });
